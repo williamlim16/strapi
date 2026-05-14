348feef41b97db0cf43e41792e0fa40025d308ed
William <williaml1601@gmail.com>
2026-05-14 14:37:33 +1000

    feat: content

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -451,6 +451,7 @@ export interface ApiBlogBlog extends Struct.CollectionTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
+    Content: Schema.Attribute.Blocks & Schema.Attribute.Required;
     Cover: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
